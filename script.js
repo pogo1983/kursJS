cc479a11472f283f1305e01be22195212de41213
Marcin Pogorzelski <mpogorzelski@Infoprojekt.pl>
2022-10-14 10:30:47 +0200

    strings

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const name = 'Pogo'
 let age = '39'
 const sport = 'Ice Hockey'
-console.log('Hej to ja, '+name+'. Mam '+age+' i lubie '+sport+'.' )
+console.log('Hej to ja, '+name+'. Mam '+age+' i lubie '+sport+'.' )//stary zapis
+console.log(`Hej to ja, ${name} i mam aktualnie ${age} lat i trenuje ${sport}` )
+
+//to check typ of data
+console.log(typeof sport)
