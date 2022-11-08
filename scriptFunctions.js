e9fc025bee38a3714280c1e3fa244e2fc3e89655
Marcin Pogorzelski <mpogorzelski@Infoprojekt.pl>
2022-11-08 21:05:47 +0100

    functions JS

--- scriptFunctions.js
+++ scriptFunctions.js
@@ -15,8 +15,32 @@
 
 //Shift + Alt + F = format code
 //Functions section from course
+
+//deklaracja funkcji
+//test() - wywolanie funckji przed jej zdefiniowaniem - przez hoisting
 function test() {
     console.log(`Hi Guys`)
 }
 test()
-//funkcja podlega hoistingowi - wszystko czytane jest od góry
+//funkcja podlega hoistingowi - wszystko czytane jest od góry, i wynoszone jest do góry
+
+//zeby nie podlegalo hoistingowi:
+//wyrazenie funkcyjne
+const helloWorld = function (){
+console.log(`Czesc bez hoitingu`)
+}
+helloWorld()
+
+//funkcja z argumentami
+function add01(params){
+    console.log(`Podany argument to: ${params}`)
+}
+add01('pogo')
+
+//parametry podajemy podczas tworzenia funckji
+//argumenty podajemy podczas wywolania funckji
+function add(x,y){
+    z = x + y 
+    console.log(`Podany argument to: ${z}`)
+}
+add(3,4)
